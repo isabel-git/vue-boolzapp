aff431827f39c2a223ad416f84345be9e4862728
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-24 11:51:20 +0100

    struttura dati ottimizzato

--- js/script.js
+++ js/script.js
@@ -6,71 +6,216 @@ var app = new Vue({
         users: [{
             name: "Michele",
             image: "img/avatar_1.jpg",
-            input: ["ciao", "kooooooo", "kkkk+"],
-            output: ["come stai?", "joooooo ooooooooooooooo ooooooooo oooooooooooooooo ggggggggggg"],
-            // message: [
-            //     {
-            //         stato: "input",
-            //         text: "ciao"
-            //     },
+            message: [
+                {
+                    status: "input",
+                    text: "ciao"
+                },
 
-            //     {
-            //         stato: "output",
-            //         text: "come stai?"
-            //     },
-            //     0}
-            //         stato: "input",
-            //         text: "tutto bene dai tu?"
-            //     },
-            // ]
+                {
+                    status: "output",
+                    text: "come stai?"
+                },
+
+                {
+                    status: "input",
+                    text: "tutto bene grazie?"
+                },
+
+                {
+                    status: "input",
+                    text: "invece tu?"
+                },
+
+                {
+                    status: "output",
+                    text: "mah non c'è male"
+                }        
+                
+            ]
         },
         {
             name: "Fabio",
             image: "img/avatar_2.jpg",
-            input: ["rooooooooo"],
-            output: ["come stai?"]
+            message: [
+                {
+                    status: "input",
+                    text: "eilaaaa"
+                },
+
+                {
+                    status: "output",
+                    text: "beellaaa"
+                },
+
+                {
+                    status: "input",
+                    text: "vgkkkkkkkk"
+                }
+
+            ]
 
         },
         {
             name: "Samuele",
             image: "img/avatar_3.jpg",
-            input: ["ciao"],
-            output: ["bffffffff"]
+            message: [
+                {
+                    status: "input",
+                    text: "we we"
+                },
+
+                {
+                    status: "input",
+                    text: "che combini?"
+                },
+
+                {
+                    status: "output",
+                    text: "weee yfgcvtvt"
+                },
+
+                {
+                    status: "input",
+                    text: "anche io"
+                }
+                        
+            ]
 
         },
         {
             name: "Mario",
             image: "img/avatar_4.jpg",
-            input: ["ciao", "ciao", "ciao"],
-            output: ["cfdtg?"]
+            message: [
+                {
+                    status: "output",
+                    text: "ciaoh"
+                },
+
+                {
+                    status: "output",
+                    text: "mih piaccionoh ih pandah"
+                }
+                
+            ]
 
         },
         {
             name: "Luigi",
             image: "img/avatar_5.jpg",
-            input: ["bla bla"],
-            output: ["come stai?"]
+            message: [
+                {
+                    status: "input",
+                    text: "shiao beloooo"
+                },
+
+                {
+                    status: "output",
+                    text: "oh"
+                },
+
+                {
+                    status: "input",
+                    text: "agdhfjdk"
+                },
+
+                {
+                    status: "input",
+                    text: "mexsaggio molto moltoo moltoooo lunho"
+                },
+
+                {
+                    status: "output",
+                    text: "bdwskskjdj lskaj"
+                }    
+                
+            ]
 
         },
         {
             name: "Luisa",
             image: "img/avatar_6.jpg",
-            input: ["lorem ipsum"],
-            output: ["tutto apposto??"]
+            message: [
+                {
+                    status: "output",
+                    text: "heiiii ciao marioo"
+                },
+
+                {
+                    status: "output",
+                    text: "oh no, scusa ho sbagliato chat"
+                },
+
+                {
+                    status: "input",
+                    text: "ahah fa niente :)"
+                },
+
+                {
+                    status: "output",
+                    text: ";)"
+                }
+
+            ]
 
         },
         {
             name: "Claudio",
             image: "img/avatar_7.jpg",
-            input: ["eiillaa"],
-            output: ["come stai?", "come stai?", "come stai?", "come stai?"]
+            message: [
+                {
+                    status: "input",
+                    text: "afagdh"
+                },
+
+                {
+                    status: "input",
+                    text: "jksdnlksn;sa"
+                },
+
+                {
+                    status: "input",
+                    text: "bskjcd aknflaj?"
+                },
+
+                {
+                    status: "output",
+                    text: "kmla klK tuD"
+                },
+
+                {
+                    status: "input",
+                    text: "jncnao jwbdybsO"
+                }    
+                
+            ]
 
         },
         {
             name: "Gio",
             image: "img/avatar_8.jpg",
-            input: ["shiao beloooo"],
-            output: ["come stai?"],
+            message: [
+                {
+                    status: "output",
+                    text: "domani a che ora?"
+                },
+
+                {
+                    status: "input",
+                    text: "alle 16:08 e 55 secondi"
+                },
+
+                {
+                    status: "output",
+                    text: "ci sarò!"
+                },
+
+                {
+                    status: "input",
+                    text: "ci conto!"
+                }
+                
+            ]
         }],
 
     },
